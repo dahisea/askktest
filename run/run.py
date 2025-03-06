72ed4128f4efd8e11d1068ea84882ee04699ab83
Dahi <81472122+dahisea@users.noreply.github.com>
2025-03-06 20:04:34 +0800

    Update run.py

--- run/run.py
+++ run/run.py
@@ -1,22 +1,13 @@
 import asyncio
+import random
 import httpx
 import uvloop
-import numpy as np
-
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 
 
 
-
-
-
-
-
-
-
-# 预定义的 User-Agent 和 IP 列表
 user_agents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
@@ -32,13 +23,12 @@ random_ips = [
     "0.0.0.0",
 ]
 
-# 预生成随机 User-Agent 和 IP 列表
-def generate_random_headers_and_ips(num_requests):
-    user_agent_list = np.random.choice(user_agents, num_requests)
-    ip_list = np.random.choice(random_ips, num_requests)
-    return list(zip(user_agent_list, ip_list))
 
-async def download_dependency(client, method, url, stats, no_response, headers):
+async def download_dependency(client, method, url, stats, no_response):
+    headers = {
+        "User-Agent": random.choice(user_agents),
+        "X-Real-IP": random.choice(random_ips)
+    }
     try:
         if method == "GET":
             response = await client.get(url, headers=headers)
@@ -58,17 +48,11 @@ async def download_dependency(client, method, url, stats, no_response, headers):
             else:
                 stats['total_responses'] += 1
     except httpx.RequestError as e:
-        print(f"Request failed: {e}")
-
-async def worker(client, queue, stats, method, no_response):
-    while not queue.empty():
-        url, headers = await queue.get()
-        await download_dependency(client, method, url, stats, no_response, headers)
-        queue.task_done()
+        print(f"Update failed: {e}")
 
 async def main():
     total_downloads = 100000
-    num_concurrent_requests = 5000  # 增加并发量
+    num_concurrent_requests = 1000
     download_method = "GET"
     no_response_needed = False
 
@@ -79,34 +63,20 @@ async def main():
         'total_response_size': 0
     }
 
-    # 预生成随机 headers 和 IPs
-    headers_and_ips = generate_random_headers_and_ips(total_downloads)
-
     # 配置连接池和复用连接以提高性能
-    limits = httpx.Limits(max_keepalive_connections=100000, max_connections=100000)
-    timeout = httpx.Timeout(timeout=30.0)  # 设置超时时间
-    async with httpx.AsyncClient(limits=limits, timeout=timeout) as client:
-        queue = asyncio.Queue()
-
-        # 填充队列
-        for headers in headers_and_ips:
-            await queue.put((dependency_url, {"User-Agent": headers[0], "X-Real-IP": headers[1]}))
-
-        # 创建并启动 worker 任务
+    limits = httpx.Limits(max_keepalive_connections=80000, max_connections=100000)
+    async with httpx.AsyncClient(limits=limits, timeout=360) as client:
+        semaphore = asyncio.Semaphore(num_concurrent_requests)
         tasks = []
-        for _ in range(num_concurrent_requests):
-            task = asyncio.create_task(worker(client, queue, download_stats, download_method, no_response_needed))
-            tasks.append(task)
 
-        # 等待队列中的所有任务完成
-        await queue.join()
+        async def sem_task():
+            async with semaphore:
+                await download_dependency(client, download_method, dependency_url, download_stats, no_response_needed)
 
-        # 取消所有 worker 任务
-        for task in tasks:
-            task.cancel()
+        for _ in range(total_downloads):
+            tasks.append(sem_task())
 
-        # 等待所有 worker 任务完成
-        await asyncio.gather(*tasks, return_exceptions=True)
+        await asyncio.gather(*tasks)
 
     print(f"总下载次数: {download_stats['total_requests']}")
     print(f"成功下载次数: {download_stats['total_responses']}")
